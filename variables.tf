37aa5c200efe996523ce302c888f75bb8d2665e6
10-TIENT-L <tient@TranTien.local>
2025-06-12 10:55:13 +0700

    chore: update subnet variable schema

--- variables.tf
+++ variables.tf
@@ -36,8 +36,12 @@ variable "vnet_dns_servers" {
 
 variable "subnets" {
   type = list(object({
-    name             = string
-    address_prefixes = list(string)
+    name                                          = string
+    address_prefixes                              = list(string)
+    default_outbound_access_enabled               = optional(bool, true)
+    private_endpoint_network_policies             = optional(string, "Disabled")
+    service_endpoints                             = optional(list(string), [])
+    private_link_service_network_policies_enabled = optional(bool, true)
     delegation = list(object({
       name = string
       service_delegation = object({
@@ -45,10 +49,6 @@ variable "subnets" {
         actions = list(string)
       })
     }))
-    default_outbound_access_enabled               = optional(bool, true)
-    private_endpoint_network_policies             = optional(string, "Disabled")
-    service_endpoints                             = optional(list(string), [])
-    private_link_service_network_policies_enabled = optional(bool, true)
   }))
 
   description = "List of subnets to create in the virtual network"
